af927731981c0d65b0af891680b690d3e5f48995
SuciAfrilia <suciafrilia19042001@gmail.com>
2023-11-08 23:51:18 +0700

    mini project Suci Afrilia

--- js/script.js
+++ js/script.js
@@ -31,7 +31,20 @@ function calculateBMI() {
     document.getElementById("bmi-result").innerHTML = "Your BMI is: " + bmi.toFixed(2);
 
     document.getElementById("page-calculator").remove();
-  
-    // Display the BMI classification to the user.
-    // document.getElementById("bmi-result").innerHTML += " (" + bmiClassification + ")";
+
+      // Classify the BMI.
+  var bmihasil = "";
+  if (bmi < 18.5) {
+    bmihasil = "Kurang Berat Badan. Cara terbaik untuk menaikkan berat badan dengan cara mengatur kalori makanan dan mengonsomsi makanan 4 sehat dan 5 sempurna serta iringi dengan olahraga";
+  } else if (bmi >= 18.5 && bmi < 25) {
+    bmihasil = "Normal (ideal). Tetap pertahankan dengan mengatur pola makanan dan menjaga olahraga serta tidur yang cukup";
+  } else if (bmi >= 25 && bmi < 29.9) {
+    bmihasil = "Kelebihan Berat Badan. Cara terbaik untuk mengurangi berat badan dengan cara mengatur kalori makan yang dikonsumsi dan berolahraga dengan teratur";
+  } else {
+    bmihasil = "Kegemukan (Obesitas). Cara terbaik untuk mengurangi berat badan dengan cara mengatur kalori makan yang dikonsumsi dan berolahraga dengan teratur";
   }
+
+  // Display the BMI classification to the user.
+  document.getElementById("bmihasil").innerHTML += " (" + bmihasil + ")";
+  
+}
